74af5743a646a675ecbf4f02e95186cb71597d29
Codigo Cristo <cristophero.alvarado@gmail.com>
2021-04-14 18:34:21 -0500

    Add files via upload

---
+++ encrypt.sh
@@ -0,0 +1,49 @@
+
+	disco="/dev/vda"
+	dd if=/dev/zero of="${disco}" bs=100M count=10 status=progress
+	parted ${disco} mklabel gpt
+	sgdisk ${disco} -n=1:0:+100M -t=1:ef00
+	sgdisk ${disco} -n=2:0:+4G -t=2:8200
+	sgdisk ${disco} -n=3:0:0
+	fdisk -l ${disco} > /tmp/partition
+	echo ""
+	cat /tmp/partition
+	sleep 3
+
+	partition="$(cat /tmp/partition | grep /dev/ | awk '{if (NR!=1) {print}}' | sed 's/*//g' | awk -F ' ' '{print $1}')"
+
+	echo $partition | awk -F ' ' '{print $1}' >  boot-efi
+	echo $partition | awk -F ' ' '{print $2}' >  swap-efi
+	echo $partition | awk -F ' ' '{print $3}' >  root-efi
+
+	echo ""
+	echo "Partición EFI es:" 
+	cat boot-efi
+	echo ""
+	echo "Partición SWAP es:"
+	cat swap-efi
+	echo ""
+	echo "Partición ROOT es:"
+	cat root-efi
+	sleep 3
+
+	clear
+	echo ""
+	echo "Formateando Particiones"
+	echo ""
+	mkfs.ext4 $(cat root-efi) 
+	mount $(cat root-efi) /mnt 
+
+	mkdir -p /mnt/efi 
+	mkfs.fat -F 32 $(cat boot-efi) 
+	mount $(cat boot-efi) /mnt/efi 
+
+	mkswap $(cat swap-efi) 
+	swapon $(cat swap-efi)
+
+	clear
+	echo ""
+	echo "Revise en punto de montaje en MOUNTPOINT"
+	echo ""
+	lsblk -l
+	sleep 3
